9c19fb6d117ff6e07085e1724d9a6d4e6ea4027b
elSchuft <wuertele@gmx.net>
2021-05-24 10:40:44 +0200

    Test superuser

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -16,9 +16,32 @@ class ModelTests(TestCase):
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
 
-    def test_new_user_email_Normalized(self):
+    def test_new_user_email_normalized(self):
         """Test the email for a new user is normalized"""
         email = 'test@LONDONAPPDEV.COM'
         user = get_user_model().objects.create_user(email, 'test123')
 
         self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
+
+    def create_superuser(self, email, password):
+        """Create and save a new super user"""
+        user = self.create_user(email ,password)
+        user.is_staff=True
+        user.is_superuser=True
+        user.save(using=self.db)
+
+        return user
+
+    def test_create_new_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@londonappdev.com',
+            'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
