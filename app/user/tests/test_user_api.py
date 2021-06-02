ea1b299878e3b254b5b35e1dc660f1d79d80b1db
elSchuft <wuertele@gmx.net>
2021-06-02 08:17:07 +0200

    linting

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -44,7 +44,9 @@ class PublicUserApiTests(TestCase):
 
     def test_password_too_short(self):
         """Test that the password must be mre than 5 characters"""
-        payload = {'email': 'test@londonappdev.com', 'password': 'pw', 'name': 'Test',}
+        payload = {
+            'email': 'test@londonappdev.com',
+            'password': 'pw', 'name': 'Test'}
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
